44132d098ef892db6849a58bb4bcd53808b7b697
tinnan <tin_nan@hotmail.com>
2016-11-09 00:15:14 +0700

    Revise screen design and add tagging main logic.

--- img_tagging/ImgTaggingForm.Designer.cs
+++ img_tagging/ImgTaggingForm.Designer.cs
@@ -28,34 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnTry = new System.Windows.Forms.Button();
+            this.btnTag = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.grpProgress = new System.Windows.Forms.GroupBox();
+            this.tagProgress = new System.Windows.Forms.ProgressBar();
+            this.pathBrowseDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtTaskLogs = new System.Windows.Forms.TextBox();
+            this.grpProgress.SuspendLayout();
             this.SuspendLayout();
             // 
-            // btnTry
+            // btnTag
             // 
-            this.btnTry.Location = new System.Drawing.Point(12, 227);
-            this.btnTry.Name = "btnTry";
-            this.btnTry.Size = new System.Drawing.Size(75, 23);
-            this.btnTry.TabIndex = 0;
-            this.btnTry.Text = "Try";
-            this.btnTry.UseVisualStyleBackColor = true;
-            this.btnTry.Click += new System.EventHandler(this.btnTry_Click);
+            this.btnTag.Location = new System.Drawing.Point(12, 45);
+            this.btnTag.Name = "btnTag";
+            this.btnTag.Size = new System.Drawing.Size(75, 23);
+            this.btnTag.TabIndex = 0;
+            this.btnTag.Text = "Tag";
+            this.btnTag.UseVisualStyleBackColor = true;
+            this.btnTag.Click += new System.EventHandler(this.btnTag_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 13);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(400, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(418, 11);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 22);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // grpProgress
+            // 
+            this.grpProgress.Controls.Add(this.txtTaskLogs);
+            this.grpProgress.Controls.Add(this.tagProgress);
+            this.grpProgress.Location = new System.Drawing.Point(13, 89);
+            this.grpProgress.Name = "grpProgress";
+            this.grpProgress.Size = new System.Drawing.Size(664, 360);
+            this.grpProgress.TabIndex = 3;
+            this.grpProgress.TabStop = false;
+            this.grpProgress.Text = "Progress";
+            // 
+            // tagProgress
+            // 
+            this.tagProgress.Location = new System.Drawing.Point(7, 20);
+            this.tagProgress.Name = "tagProgress";
+            this.tagProgress.Size = new System.Drawing.Size(651, 23);
+            this.tagProgress.TabIndex = 0;
+            // 
+            // pathBrowseDialog
+            // 
+            this.pathBrowseDialog.Description = "Select root folder to start tagging.";
+            this.pathBrowseDialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.pathBrowseDialog.ShowNewFolderButton = false;
+            this.pathBrowseDialog.HelpRequest += new System.EventHandler(this.pathBrowseDialog_HelpRequest);
+            // 
+            // txtTaskLogs
+            // 
+            this.txtTaskLogs.Location = new System.Drawing.Point(7, 50);
+            this.txtTaskLogs.Multiline = true;
+            this.txtTaskLogs.Name = "txtTaskLogs";
+            this.txtTaskLogs.ReadOnly = true;
+            this.txtTaskLogs.Size = new System.Drawing.Size(651, 304);
+            this.txtTaskLogs.TabIndex = 1;
             // 
             // ImgTaggingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.btnTry);
+            this.ClientSize = new System.Drawing.Size(689, 461);
+            this.Controls.Add(this.grpProgress);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnTag);
             this.Name = "ImgTaggingForm";
             this.Text = "Image tagging";
+            this.grpProgress.ResumeLayout(false);
+            this.grpProgress.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btnTry;
+        private System.Windows.Forms.Button btnTag;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.GroupBox grpProgress;
+        private System.Windows.Forms.ProgressBar tagProgress;
+        private System.Windows.Forms.FolderBrowserDialog pathBrowseDialog;
+        private System.Windows.Forms.TextBox txtTaskLogs;
     }
 }
 
--- img_tagging/ImgTaggingForm.cs
+++ img_tagging/ImgTaggingForm.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+using System.IO;
 
 namespace img_tagging
 {
@@ -18,21 +19,93 @@ namespace img_tagging
             InitializeComponent();
         }
 
-        private void btnTry_Click(object sender, EventArgs e)
+        // Archive directory name.
+        private const string ARCHIVE_DIR = "archive";
+        // Tags library file, stores tags and their members.
+        private const string TAGS_LIBRARY_FILE = "tags.json";
+
+        private void btnTag_Click(object sender, EventArgs e)
         {
-            string[] tags = new string[3] { "Good", "Bad", "Nah" };
-            // read file.
-            var shellobj = Microsoft.WindowsAPICodePack.Shell.ShellObject.FromParsingName("C:\\Users\\ntin\\Pictures\\nishi\\tumblr_adventure_1280.jpg");
-            // print Tags property to file.
-            foreach (string tag in shellobj.Properties.System.Keywords.Value) {
-                Console.WriteLine(tag);
+            //string[] tags = new string[3] { "Good", "Bad", "Nah" };
+            //// read file.
+            //var shellobj = Microsoft.WindowsAPICodePack.Shell.ShellObject.FromParsingName("C:\\Users\\ntin\\Pictures\\nishi\\tumblr_adventure_1280.jpg");
+            //// print Tags property to file.
+            //foreach (string tag in shellobj.Properties.System.Keywords.Value)
+            //{
+            //    Console.WriteLine(tag);
+            //}
+            //// get writer of the property.
+            //ShellPropertyWriter w = shellobj.Properties.GetPropertyWriter();
+            //// write new value of the property.
+            //w.WriteProperty(SystemProperties.System.Keywords, tags);
+            //// close the writer.
+            //w.Close();
+
+            tag(txtPath.Text);
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = pathBrowseDialog.ShowDialog();
+            
+            if(result == DialogResult.OK 
+                && !String.IsNullOrWhiteSpace(pathBrowseDialog.SelectedPath))
+            {
+                txtPath.Text = pathBrowseDialog.SelectedPath;
             }
-            // get writer of the property.
-            ShellPropertyWriter w = shellobj.Properties.GetPropertyWriter();
-            // write new value of the property.
-            w.WriteProperty(SystemProperties.System.Keywords, tags);
-            // close the writer.
-            w.Close();
+        }
+
+        private void pathBrowseDialog_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tag(string rootPath)
+        {
+            // List all directory found in the rootPath except for ARCHIVE_DIR.
+            var dirs = Directory.GetDirectories(rootPath).Where(d => Path.GetFileName(d) != ARCHIVE_DIR);
+            // Clear the task logs.
+            txtTaskLogs.Clear();
+            // Clear the progress bar.
+            tagProgress.Value = 0;
+
+            if(dirs.ToArray().Length == 0)
+            {
+                // No directory is found!
+                log("Not found directory.");
+                return; // End this process.
+            }
+
+            int task_count = dirs.ToArray().Length;
+            int task_done = 0;
+
+            foreach(string d in dirs)
+            {
+                tagdir(d);
+
+                task_done++; // Increase task_done count.
+                updateTaskProgressBar(task_done, task_count);
+            }
+        }
+
+        private void log(string msg)
+        {
+            if (txtTaskLogs.Text.Length != 0)
+            {
+                txtTaskLogs.AppendText("\r\n");
+            }
+            txtTaskLogs.AppendText(msg);
+        }
+
+        private void updateTaskProgressBar(int done, int all)
+        {
+            tagProgress.Value = (done / all) * 100;
+        }
+
+        private void tagdir(string path)
+        {
+            log("Tagging images in directory: " + path);
+
         }
     }
 }
